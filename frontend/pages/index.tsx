678a2c25d366c684f7b3b6adde0de45ea34fc939
Daniel <dnnxzz70@gmail.com>
2024-09-12 03:43:22 -0300

    Changes to be committed: 	modified:   .gitignore 	new file:   frontend/components/HeroHeader 	modified:   frontend/pages/index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,12 @@
 import type { NextPage } from 'next';
 import HeaderMain from '../components/Header/HeaderMain';
+import HeroHeader from '../components/HeroHeader';
 
 const Home: NextPage = () => {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-100">
       <HeaderMain />
+      <HeroHeader />
     </div>
   );
 };
