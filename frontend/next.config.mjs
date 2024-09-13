c4c863eb7ecfc62809f7f8a00113ba8f2b65193e
Daniel <dnnxzz70@gmail.com>
2024-09-13 16:19:47 -0300

    Changes to be committed: 	modified:   frontend/next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,9 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['images.unsplash.com', 'images.pexels.com', 'source.unsplash.com', 'images.pexels.com'],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    domains: ['images.unsplash.com', 'images.pexels.com', 'source.unsplash.com', 'images.pexels.com'],
+  },
+  async redirects() {
+    return [
+      // Redireciona de danielneri.online para www.danielneri.online
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: 'danielneri.online' }],
+        destination: 'https://www.danielneri.online/:path*',
+        permanent: true,
+      },
+    ];
+  },
+};
+
+export default nextConfig;
