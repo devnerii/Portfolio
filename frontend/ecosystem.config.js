cdd461f4972ed026b99871a26867f946cc3527e4
Daniel <dnnxzz70@gmail.com>
2024-09-17 17:22:31 -0300

    modified:   frontend/ecosystem.config.js

--- frontend/ecosystem.config.js
+++ frontend/ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'my-app',
+      name: 'Portoflio',
       script: 'npm',
       args: 'start',
       instances: 'max', // Usa o máximo de instâncias disponíveis (modo cluster)
